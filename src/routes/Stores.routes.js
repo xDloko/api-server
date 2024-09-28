fc0cbf5f559469d8ce020a7544a01fa12dd9b2af
xDloko <70291796+xDloko@users.noreply.github.com>
2024-09-28 12:14:15 -0500

    Update Stores.routes.js

--- src/routes/Stores.routes.js
+++ src/routes/Stores.routes.js
@@ -9,6 +9,6 @@ router.post("/producto", registrarProducto)
 router.post("/productos-update", storeRequired, añadirProductos)
 router.post("/tienda-login", login)
 router.post("/tienda-logout", storelogout)
-router.post("/tienda-all", obtenerTiendas)
+router.get("/tienda-all", obtenerTiendas)
 
 export default router;
