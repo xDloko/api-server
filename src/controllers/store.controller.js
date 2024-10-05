5bf04aee032bc651460e0b6094704b0b6d082f19
xDloko <santiagopcuenca18@gmail.com>
2024-10-05 16:03:40 -0500

    Che arreglo menu_id en obtener productos

--- src/controllers/store.controller.js
+++ src/controllers/store.controller.js
@@ -189,8 +189,8 @@ export const editarProducto = async (req, res) => {
 
 export const obtenerProductos= async ( req, res )=>{
   try {
-    const { menu } = req.body
-    const productoFind = await Producto.find({ menu });
+    const { menu_id } = req.body
+    const productoFind = await Producto.find({ menu_id });
     if (!productoFind.length) {
       return res.status(404).json({ message: 'No se encontraron productos en este menu' });
     }
