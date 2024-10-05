22f137be455d33ac6dda14e4da432b8cbf64a612
xDloko <santiagopcuenca18@gmail.com>
2024-10-04 23:10:43 -0500

    Correccion MenuMode

--- src/models/menu.model.js
+++ src/models/menu.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Menu = new mongoose.Schema({
   tienda_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Tienda', required: true },
   name: { type: String, required: true },
-  descripcion: { type: String },
+  descripccion: { type: String },
   categoria: { type: String, enum: ['Fritos', 'Helado', 'Pasa-Bocas'], required: true }
   
 });
