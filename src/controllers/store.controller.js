aecf1cd32e307da5a2800e2ac667a0cc603169fb
xDloko <santiagopcuenca18@gmail.com>
2024-10-04 20:36:36 -0500

    Omagad

--- src/controllers/store.controller.js
+++ src/controllers/store.controller.js
@@ -74,13 +74,14 @@ export const storelogout = (req, res)=>{
 /** Menus  */
 
 export const crearMenu = async (req, res) => {
-  const { tienda_id, name, descripccion } = req.body;
+  const { tienda_id, name, descripccion, categoria } = req.body;
 
   try {
     const newMenu = new Menu({
       tienda_id,
       name,
       descripccion,
+      categoria,
     });
 
     await newMenu.save();
