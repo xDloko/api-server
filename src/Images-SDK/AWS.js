fb908f3dc1d13115dd10e422fef2ce5e4963f112
xDloko <santiagopcuenca18@gmail.com>
2024-10-18 08:53:14 -0500

    che

--- src/Images-SDK/AWS.js
+++ src/Images-SDK/AWS.js
@@ -5,13 +5,13 @@ dotenv.config();
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
+    region: process.env.AWS_S3_REGION
   });
   
   // Definir la función uploadToS3
   export const uploadToS3 = async (file) => {
     const params = {
-      Bucket: process.env.AWS_BUCKET_NAME,  // Nombre del bucket
+      Bucket: process.env.AWS_S3_BUCKET_NAME,  // Nombre del bucket
       Key: `uploads/${Date.now()}_${file.originalname}`,  // Ruta del archivo en S3
       Body: file.buffer,
       ContentType: file.mimetype
