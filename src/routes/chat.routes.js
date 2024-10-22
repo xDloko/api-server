5d4013f0dbb9c060bb69d058db40ac4212c7ae37
PrimalSense <elfelpasgg@gmail.com>
2024-10-22 01:39:59 +0000

    chats v2

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -8,7 +8,6 @@ router.get('/chat/getmessages', getMessages);
 
 // Ruta para enviar un mensaje
 router.post('/chat/sendmessages', sendMessage);
-router.post('/chat/sendmessage', sendMessage);
 
 export default router;
 
