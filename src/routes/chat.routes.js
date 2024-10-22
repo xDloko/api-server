f0f922defb203b2d96cfecbf028e2f5f94b0c78d
PrimalSense <elfelpasgg@gmail.com>
2024-10-22 02:19:33 +0000

    ddd

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -4,10 +4,10 @@ import { getMessages, sendMessage} from '../controllers/chat.controller'
 // Ruta para obtener mensajes
 
 const router = Router();
-router.get('/getmessages', getMessages);
+router.get("/getmessages", getMessages);
 
 // Ruta para enviar un mensaje
-router.post('/sendmessages', sendMessage);
+router.post("/sendmessage", sendMessage);
 
 export default router;
 
