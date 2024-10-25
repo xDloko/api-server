9e84ade067f30430b8dc333fb8e086788aed68fd
xDloko <santiagopcuenca18@gmail.com>
2024-10-24 19:34:53 -0500

    Creacion se servidor http + socket.io para chats

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import authRoutes from './routes/auth.routes.js'
 import authstoreRoutes from './routes/Stores.routes.js'
 import systemRoutes from './routes/system.routes.js'
 import cors from 'cors'
+import { Server as SocketIo } from 'socket.io';
+import mongoose from 'mongoose'
+import Message from './models/chat.model.js';
 
 const app = express()
 app.use(morgan('dev'));
@@ -16,6 +19,43 @@ app.use("/api/user",authRoutes);
 app.use("/api/store",authstoreRoutes);
 app.use("/api/system",systemRoutes);
 
+const server = http.createServer(app);
 
+const io = new SocketIo(server, {
+    cors: {
+      origin: "http://api-server-production-12d3.up.railway.app", 
+      methods: ["GET", "POST"]
+    }
+  });
+
+mongoose.connect('mongodb+srv://felpssense:aplication@senses.nbg2v.mongodb.net/?retryWrites=true&w=majority&appName=Senses', { 
+useNewUrlParser: true, 
+useUnifiedTopology: true 
+});
+
+io.on('connection', (socket) => {
+    console.log('Un usuario se ha conectado');
+
+    socket.on('nuevo_mensaje', async (data) => {
+      const nuevoMensaje = new Mensaje({
+        texto: data.texto,
+        fecha: new Date(),
+      });
+      await nuevoMensaje.save();
+      io.emit('actualizar_mensajes', data);
+    });
+  
+    //desconexión
+    socket.on('disconnect', () => {
+      console.log('Usuario desconectado');
+    });
+  });
+  
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    }
+);
+  
 
 export default app;
